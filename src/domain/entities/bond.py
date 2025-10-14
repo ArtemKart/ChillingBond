015d8bfc338db35bd60ed3c0552ca7717252f3d8
Artem Kartashov <artem.kartashov@idea.edu.pl>
2025-10-14 23:48:37 +0200

    implementation of 3 entities for now: User, Bond and ReferenceRate

---
+++ src/domain/entities/bond.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from src.domain.exceptions import BondDomainError
+
+
+@dataclass
+class Bond:
+    """ Represents the bond
+    Args:
+        id (int): Bond identifier.
+        nominal_value (float): Nominal value of a single bond unit.
+        series (str): Identifier or code representing the bond series.
+        maturity_period (int): Total lifetime of the bond, typically expressed
+            in months.
+        initial_interest_rate (float): Interest rate applied during the initial
+            period of the bond.
+        first_interest_period (int): Duration of the initial interest rate period,
+            typically expressed in months.
+        reference_rate_margin (float): Additional percentage added to the reference rate (spread).
+    """
+
+    id: int
+    nominal_value: float
+    series: str
+    maturity_period: int
+    initial_interest_rate: float
+    first_interest_period: int
+    reference_rate_margin: float
+
+
+    def _check_initial_period_duration(self) -> None:
+        if self.first_interest_period > self.maturity_period:
+            error_msg = "First Interest period cannot be larger that bond maturity period"
+            raise BondDomainError(error_msg)
---
+++ src/domain/entities/user.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    """ Represents the user
+    Args:
+        id (int): User identifier.
+        email (str): User email.
+        password (str): User password.
+        name (str, None): User first name.
+    """
+
+    id: int
+    email: str
+    password: str
+    name: str | None
