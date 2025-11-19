1f5c8b84d13eb147a43386ea9baf69f9482e3c7e
Artem Kartashov <artem.kartashov@idea.edu.pl>
2025-11-19 20:13:17 +0100

    add monitoring utils

--- src/adapters/inbound/api/main.py
+++ src/adapters/inbound/api/main.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.adapters.exceptions import SQLAlchemyRepositoryError
 from src.adapters.inbound.api.exception_handlers import (
@@ -15,9 +16,21 @@ from src.domain.exceptions import DomainError
 
 app: Final = FastAPI()
 
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    should_ignore_untemplated=True,
+    should_respect_env_var=False,
+    should_instrument_requests_inprogress=True,
+    excluded_handlers=["/metrics"],
+    inprogress_name="inprogress",
+    inprogress_labels=True,
+)
+instrumentator.instrument(app).expose(app, endpoint="/metrics")
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
