2ed78f3ba8e6d105832d717937f45dbec0dcf34f
ArtemKart <94437123+ArtemKart@users.noreply.github.com>
2026-01-20 14:27:18 +0100

    Deploy (#32)
    * setup deploy environment

---
+++ start-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+export BACKEND_HOST=${BACKEND_HOST:-backend1.railway.internal}
+export BACKEND_PORT=${BACKEND_PORT:-8080}
+export FRONTEND_HOST=${FRONTEND_HOST:-frontend1.railway.internal}
+export FRONTEND_PORT=${FRONTEND_PORT:-8080}
+export PORT=${PORT:-80}
+
+envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT} ${PORT}' \
+  < /etc/nginx/nginx.conf.template \
+  > /etc/nginx/nginx.conf
+
+echo "✅ Generated nginx.conf"
+
+exec nginx -g 'daemon off;'
