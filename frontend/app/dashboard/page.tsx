8f46df18b736461e4ec624448fd3898b366bc35b
Artem Kartashov <artem.kartashov@idea.edu.pl>
2026-02-13 01:40:30 +0100

    fix dashboard flicks when not authenticated

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { BondsTab } from "@/app/dashboard/components/BondsTab";
 import { ChartsTab } from "@/app/dashboard/components/ChartsTab";
 import { Sidebar } from "@/app/dashboard/components/SideBar";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 import { TabType } from "@/types/TabType";
 import { useState } from "react";
 
@@ -10,14 +11,16 @@ export default function DashboardPage() {
     const [activeTab, setActiveTab] = useState<TabType>("bonds");
 
     return (
-        <div className="min-h-screen bg-gray-50">
-            <div className="flex">
-                <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
-                <main className="flex-1 p-8">
-                    {activeTab === "bonds" && <BondsTab />}
-                    {activeTab === "charts" && <ChartsTab />}
-                </main>
+        <ProtectedRoute>
+            <div className="min-h-screen bg-gray-50">
+                <div className="flex">
+                    <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
+                    <main className="flex-1 p-8">
+                        {activeTab === "bonds" && <BondsTab />}
+                        {activeTab === "charts" && <ChartsTab />}
+                    </main>
+                </div>
             </div>
-        </div>
+        </ProtectedRoute>
     );
 }
